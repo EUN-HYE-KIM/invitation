52a3c73aaa3ea9395af4c8fadd416833a2491c4a
EUN-HYE-KIM <dmsp1234@gmail.com>
2024-09-18 02:08:01 +0900

    초기 세팅

---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  nitro: {
+    preset : 'github-pages' 
+  },
+  app: {
+    baseURL : '/my-repository-url/',
+    head: {
+        title: "승민 ♥ 은하 결혼합니다 :)",
+        htmlAttrs: {
+            lang: "ko",
+        },
+        meta: [
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content:
+                    "width=device-width,initial-scale=1, maximum-scale=1.0, user-scalable=no",
+            },
+        ],
+    },
+  },
+  vite: {
+      css: {
+          preprocessorOptions: {
+              scss: {
+                  additionalData: `@use "@/assets/css/abstracts/variables" as *; @import "@/assets/css/abstracts/mixin";`,
+              },
+          },
+      },
+  },
+  css: ["@/assets/css/main.scss"],
+  devtools: { enabled: false },
+});
